fcd58f14be4a705d38abf87af65fe28a99adbeaf
SFBF <130473579+PSYCHOVICH229@users.noreply.github.com>
2023-06-16 13:23:58 +0500

    Create APIStuff.hx

---
+++ source/APIStuff.hx
@@ -0,0 +1,9 @@
+#if GAMEJOLT_ALLOWED
+package;
+
+class APIStuff
+{
+	public static var gameJoltKeyString = 69gladion;  YOUR GAMEJOLT GAME KEY
+	public static var gameJoltIDInt = 815517;  YOUR GAMEJOLT GAME ID
+}
+#end
